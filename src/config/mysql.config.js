7c5141db9ae26b5b977a8a303159f8e0753a8d59
elHenryettas <92875883+elHenryettas@users.noreply.github.com>
2022-06-08 01:28:22 -0300

    34

---
+++ src/config/mysql.config.js
@@ -0,0 +1,15 @@
+import mysql from "mysql";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const pool = mysql.createPool({
+  host: process.env.BD_HOST,
+  port: process.env.BD_PORT,
+  user: process.env.BD_USER,
+  password: process.env.BD_PASSWORD,
+  database: process.env.BD_NAME,
+  connectionLimit: process.env.BD_CONNECTION_LIMIT,
+});
+
+export default pool;
